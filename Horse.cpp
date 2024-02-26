09ee6e7e59618e6f0ad648b6f9efab906193cc00
Farokh Mahmoodi <72624489+farokhmahmoodi@users.noreply.github.com>
2024-02-26 08:12:05 -0600

    Update Horse.cpp

--- Horse.cpp
+++ Horse.cpp
@@ -3,7 +3,7 @@
 Horse::Horse()
 {
 	name = "", rider = "";
-	distanceTraveled = 0;
+	distanceTraveled = 0, racesWon = 0;
 	unsigned seed;
 	seed = time(0);
 	srand(seed);
@@ -13,7 +13,7 @@ Horse::Horse()
 Horse::Horse(string horseName, string riderName)
 {
 	name = horseName, rider = riderName;
-	distanceTraveled = 0;
+	distanceTraveled = 0, racesWon = 0;
 	unsigned seed;
 	seed = time(0);
 	srand(seed);
@@ -36,8 +36,12 @@ void Horse::sendToGate()
 void Horse::displayHorse(int goalLength)
 {
 	cout << "|";
-	for (int i = 0; i < distanceTraveled; i++)
-		cout << "-";
-	//cout << setw(distanceTraveled) << ">";
-	cout << setw(goalLength) << "|" << endl;
+	for (int i = 0; i <= distanceTraveled; i++)
+	{
+		if (i == distanceTraveled)
+			cout << ">";
+		else
+			cout << "-";
+	}
+	cout << setw(goalLength - distanceTraveled) << "|" << endl;
 }
